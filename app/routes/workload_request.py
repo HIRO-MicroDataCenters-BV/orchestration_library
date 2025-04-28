2c6568dd01204708849d5c9c0d63e77893b5d0fa
renuka <renukagollu77@gmail.com>
2025-04-28 13:11:19 +0200

    workload_request in routes

--- app/routes/workload_request.py
+++ app/routes/workload_request.py
@@ -10,7 +10,50 @@ router = APIRouter(prefix="/workload_request")
 # def create(data: schemas.WorkloadRequestCreate, db: Session = Depends(get_db)):
 #     return crud.create_workload_request(db, data)
 
+
 @router.post("/")
-async def create(data: schemas.WorkloadRequestCreate, db: AsyncSession = Depends(get_async_db)):
+async def create(
+    data: schemas.WorkloadRequestCreate, db: AsyncSession = Depends(get_async_db)
+):
     return await crud.create_workload_request(db, data)
 
+
+@router.get("/")
+async def read_workload_requests(
+    name: str = None,
+    namespace: str = None,
+    api_version: str = None,
+    kind: str = None,
+    current_scale: int = None,
+    db: AsyncSession = Depends(get_async_db),
+):
+    workloads = await crud.get_workload_requests(
+        db,
+        name=name,
+        namespace=namespace,
+        api_version=api_version,
+        kind=kind,
+        current_scale=current_scale,
+    )
+    if not workloads:
+        return {"error": "No workload requests found"}
+    return workloads
+
+
+@router.put("/{workload_request_id}")
+async def update_workload_request(
+    workload_request_id: int,
+    data: schemas.WorkloadRequestUpdate,
+    db: AsyncSession = Depends(get_async_db),
+):
+    return await crud.update_workload_request(
+        db, workload_request_id, updates=data.model_dump(exclude_unset=True)
+    )
+
+
+@router.delete("/{workload_request_id}")
+async def delete_workload_request(
+    workload_request_id: int,
+    db: AsyncSession = Depends(get_async_db),
+):
+    return await crud.delete_workload_request(db, workload_request_id)
