c2d7fb05a18dd73ece680bc05c0d1027b61870be
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M4.local>
2025-04-29 14:24:26 +0200

    Refactored the code

--- app/routes/pod.py
+++ app/routes/pod.py
@@ -7,22 +7,22 @@ from app.schemas import PodCreate, PodUpdate
 
 router = APIRouter(prefix="/pod")
 
-@router.post("/", response_model=dict)
+@router.post("/")
 async def create(data: PodCreate, db: AsyncSession = Depends(get_async_db)):
     return await crud.create_pod(db, data)
 
-@router.get("/", response_model=list)
+@router.get("/")
 async def get(db: AsyncSession = Depends(get_async_db)):
     return await crud.get_pod(db)
 
-@router.get("/{pod_id}", response_model=dict)
+@router.get("/{pod_id}")
 async def get(pod_id: int, db: AsyncSession = Depends(get_async_db)):
     return await crud.get_pod(db, pod_id)
 
-@router.put("/{pod_id}", response_model=dict)
+@router.put("/{pod_id}")
 async def update(pod_id: int, data: PodUpdate, db: AsyncSession = Depends(get_async_db)):
     return await crud.update_pod(db, pod_id, data)
 
-@router.delete("/{pod_id}", response_model=dict)
+@router.delete("/{pod_id}")
 async def delete(pod_id: int, db: AsyncSession = Depends(get_async_db)):
     return await crud.delete_pod(db, pod_id)
