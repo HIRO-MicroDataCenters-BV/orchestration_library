893b60ae3187b6f0c088bbe73a7d2ca194ef59ae
rui <rui.min@hiro-microdatacenters.nl>
2025-05-06 07:49:00 +0200

    modified the pods list

--- app/kubernetes_info/list_pods.py
+++ app/kubernetes_info/list_pods.py
@@ -3,11 +3,16 @@
 #pip install kubernetes
 import kubernetes.client
 from kubernetes import client, config
-
-config.load_kube_config("/root/config")   # I'm using file named "config" in the "/root" directory
+config.load_incluster_config()
+config.load_kube_config("/root/config")
 
 v1 = kubernetes.client.CoreV1Api()
 print("Listing pods with their IPs:")
+
 ret = v1.list_pod_for_all_namespaces(watch=False)
+# a specific namespace
+ret = v1.list_namespaced_pod(namespace="dev", watch=False)
+
 for i in ret.items:
     print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+   
