7ce29b2b1ea624280579255303ea9e96b97a6891
rui <rui.min@hiro-microdatacenters.nl>
2025-05-06 08:11:38 +0200

    modified the pod api

--- app/kubernetes_info/list_pods_apis.py
+++ app/kubernetes_info/list_pods_apis.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
-from app.kubernetes_info import list_pods
+from list_pods import list_pods
 
 
-router = APIRouter(prefix="/kubernetes")
-@router.get("/")
+router = APIRouter(prefix="/kpods")
+@router.get("/list_all")
 def list_all_pods():
-    return list_pod_for_all_namespaces()
+    return list_pods.list_pod_for_all_namespaces()
