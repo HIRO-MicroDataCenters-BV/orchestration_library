a0051319010cb77a1efe6af61637821fddc22037
rui <rui.min@hiro-microdatacenters.nl>
2025-05-28 23:05:17 +0200

    added some logic

--- app/api/k8s/cluster_ui.py
+++ app/api/k8s/cluster_ui.py
@@ -5,7 +5,6 @@ Cluster statistic Info API
 from fastapi import APIRouter
 from app.repositories.k8s import k8s_cluster_info
 
-
 router = APIRouter(prefix="/ui_cluster_info")
 
 
@@ -14,4 +13,19 @@ def get_ui_cluster_statistic_info():
     """
     Get statistic data from cluster and other APIS
     """
-    return k8s_cluster_info.get_cluster_info()
+    k8s_cluster_info.cluster_info = k8s_cluster_info.get_cluster_info()
+
+    """
+    loop the nodes of the cluster , get each node usage, and calculate the cluster usage
+    """
+    cluster_usage = 0;
+
+
+    cluster_statistic_info = {
+        "cluster_info": k8s_cluster_info.cluster_info,
+        "cluster_usage": cluster_usage
+    }
+
+
+
+    return cluster_statistic_info;
