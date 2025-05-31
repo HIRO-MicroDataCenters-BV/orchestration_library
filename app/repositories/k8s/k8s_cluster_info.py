c512f398eb57d777c5a01adb6b339c5172f8e530
S S Krishna Vutukuri <sskrishnavutukuri@Mac.fritz.box>
2025-05-31 22:08:33 +0200

    cluster id from uid of kube-system

--- app/repositories/k8s/k8s_cluster_info.py
+++ app/repositories/k8s/k8s_cluster_info.py
@@ -2,7 +2,9 @@
 Get cluster information from Kubernetes.
 """
 
+import boto3
 from kubernetes.client.exceptions import ApiException
+from kubernetes import config
 
 from app.repositories.k8s.k8s_common import (get_k8s_core_v1_client,
                                          get_k8s_version_api_client)
@@ -57,12 +59,33 @@ def get_cluster_info():
     for pod in kube_system_pods:
         kube_system_pods_info.append(get_pod_basic_info(pod))
 
+    # Get cluster id
+    try:
+        # The cluster ID is typically the UID of the kube-system namespace
+        cluster_id = core_v1.read_namespace(name="kube-system").metadata.uid
+    except ApiException as e:
+        cluster_id = None
+        print(f"Error fetching cluster ID: {e}")
+
+    # # Get cluster name
+    # try:
+    #     config.load_incluster_config()
+    #     contexts, active_context = config.list_kube_config_contexts()
+    #     print(f"Contexts: {contexts}")
+    #     print(f"Active context: {active_context}")
+    #     cluster_name = active_context['context']['cluster']
+    # except config.ConfigException as e:
+    #     cluster_name = None
+    #     print(f"Error fetching cluster name: {e}")
+
+
     cluster_info = {
         "kubernetes_version": version_info.git_version,
         "nodes": nodes,
         "components": component_status,
         "kube_system_pods": kube_system_pods_info,
-        "cluster_id": version_info.git_commit
+        "cluster_id": cluster_id,
+        "cluster_name": cluster_name,
     }
 
     return cluster_info
