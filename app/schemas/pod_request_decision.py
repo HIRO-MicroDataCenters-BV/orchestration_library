49bf014c8b5f346dd25954b78e3484ab8f1929b8
renuka <renukagollu77@gmail.com>
2025-06-11 07:47:44 +0200

    adding new table pod request decision

---
+++ app/schemas/pod_request_decision.py
@@ -0,0 +1,33 @@
+"""
+Schemas for the API requests and responses.
+"""
+
+from pydantic import BaseModel
+from uuid import UUID
+from typing import Optional
+from datetime import datetime
+
+
+class PodRequestDecisionSchema(BaseModel):
+    """
+    Schema for pod request decision.
+    """
+    id: UUID
+    pod_id: UUID
+    pod_name: str
+    namespace: str
+    node_id: UUID
+    is_elastic: bool
+    queue_name: str
+    demand_cpu: float
+    demand_memory: float
+    demand_slack_cpu: Optional[float] = None
+    demand_slack_memory: Optional[float] = None
+    is_decision_status: bool
+    pod_parent_id: UUID
+    pod_parent_kind: str
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
