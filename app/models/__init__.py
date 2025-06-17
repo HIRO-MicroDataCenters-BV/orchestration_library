ff0a0e8c67b3994a65add26a51f782142c3a437a
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M4.local>
2025-06-17 18:44:36 +0200

    alembic compatable changes

---
+++ app/models/__init__.py
@@ -0,0 +1,11 @@
+import os
+import glob
+
+# Get the directory of this file
+models_dir = os.path.dirname(__file__)
+
+# Import all .py files except __init__.py and files starting with '_'
+for file in glob.glob(os.path.join(models_dir, "*.py")):
+    module = os.path.basename(file)[:-3]
+    if module not in ("__init__", "base_dict_mixin") and not module.startswith("_"):
+        __import__(f"{__name__}.{module}")
