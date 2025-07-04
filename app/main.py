5ea8db644994a0d7f6a9310400e3f70822155d45
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M4.local>
2025-07-04 02:07:31 +0200

    New api for worklflow decission and action

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ from app.api import (
     workload_request_decision_api,
 )
 
+from app.repositories import workload_flow
 from app.utils.exception_handlers import init_exception_handlers
 
 app = FastAPI()
@@ -47,6 +48,7 @@ app.include_router(tuning_parameters_api.router, tags=["Tuning Parameters"])
 app.include_router(workload_request_decision_api.router, tags=["Workload Request Decision"])
 app.include_router(alerts_api.router, tags=["Alerts API"])
 app.include_router(workload_action_api.router, tags=["Workload Action"])
+app.include_router(workload_flow.router, tags=["Workload Flow"])
 
 app.include_router(k8s_dashboard_api.router, tags=["Kubernetes Dashboard"])
 
