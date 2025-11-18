4ebc07d4ee2384fe22181dacd8847088465959bc
S S Krishna Vutukuri <sskrishnavutukuri@mac.home>
2025-11-18 14:00:33 +0100

    Add API to get latest KPI metrics and tuning prams together

--- app/repositories/kpi_metrics_geometric_mean.py
+++ app/repositories/kpi_metrics_geometric_mean.py
@@ -13,6 +13,7 @@ from sqlalchemy.exc import SQLAlchemyError
 
 from app.metrics.helper import record_api_metrics
 from app.models.kpi_metrics_geometric_mean import KPIMetricsGeometricMean
+from app.models.tuning_parameter import TuningParameter
 from app.schemas.kpi_metrics_geometric_mean_schema import KPIMetricsGeometricMeanItem
 from app.utils.exceptions import DatabaseConnectionException
 
@@ -36,7 +37,7 @@ async def fetch_latest_geometric_mean_kpis(
             skip (int): The number of entries to skip.
             limit (int): The number of latest entries to retrieve.
         metrics_details (dict): Details for metrics logging.
-        
+
 
     Returns:
         List[KPIMetricsGeometricMeanItem]: The latest geometric mean KPI metrics entries.
@@ -48,7 +49,9 @@ async def fetch_latest_geometric_mean_kpis(
     try:
         skip = kpi_geometrics_request_args.pop("skip", 0)
         limit = kpi_geometrics_request_args.pop("limit", 10)
-        request_decision_id: UUID = kpi_geometrics_request_args.get("request_decision_id")
+        request_decision_id: UUID = kpi_geometrics_request_args.get(
+            "request_decision_id"
+        )
         if request_decision_id:
             query = select(KPIMetricsGeometricMean).where(
                 KPIMetricsGeometricMean.request_decision_id == request_decision_id
@@ -92,3 +95,70 @@ async def fetch_latest_geometric_mean_kpis(
             record_api_metrics(
                 metrics_details=metrics_details, status_code=503, exception=excep
             )
+
+
+async def get_latest_geometric_mean_kpis_tuning_parameters(
+    db_session: AsyncSession, limit: int
+):
+    """
+    Get the latest geometric mean KPI metrics entries for tuning parameters.
+
+    Args:
+        limit (int): The number of latest entries to retrieve.
+    Returns:
+        List of the latest geometric mean KPI metrics entries.
+    """
+    try:
+        K = KPIMetricsGeometricMean
+        P = TuningParameter
+
+        # Subquery to get the latest tuning parameter for each KPI metric
+        subquery = (
+            select(P)
+            .where(P.created_at <= K.last_created_at)
+            .order_by(P.created_at.desc())
+            .limit(1)
+            .lateral()
+        )
+
+        query = (
+            select(
+                K.last_created_at,
+                K.gm_cpu_utilization,
+                K.gm_mem_utilization,
+                K.gm_decision_time_in_seconds,
+                K.request_decision_id,
+                P.alpha,
+                P.beta,
+                P.gamma,
+                P.output_1,
+                P.output_2,
+                P.output_3,
+                P.created_at,
+            )
+            .select_from(K)
+            .join(subquery, True, isouter=True)
+            .order_by(K.last_created_at.desc())
+            .limit(limit)
+        )
+
+        kpi_geometrics_tuning_params_result = await db_session.execute(query)
+        return kpi_geometrics_tuning_params_result.all()
+    except SQLAlchemyError as e:
+        logger.error(
+            "Database error while fetching latest geometric mean KPI metrics for tuning parameters: %s",
+            str(e),
+        )
+        raise DatabaseConnectionException(
+            "Failed to fetch latest geometric mean KPI metrics for tuning parameters",
+            details={"error": str(e)},
+        ) from e
+    except Exception as e:
+        logger.error(
+            "Unexpected error while fetching latest geometric mean KPI metrics for tuning parameters: %s",
+            str(e),
+        )
+        raise DatabaseConnectionException(
+            "An unexpected error occurred while fetching latest geometric mean KPI metrics for tuning parameters",
+            details={"error": str(e)},
+        ) from e
